698b4763d3e2e100a39183d3e517d4c96222c56a
Deividwb <bernardinodeivid@gmail.com>
2022-02-27 19:19:20 -0300

    Projeto Inicializado

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Reqres from './component/reqres';
+
+function App() {
+  return (
+    <div>
+   <h1>ola mundo</h1>
+   <Reqres/>
+   </div>
+  );
+}
+
+export default App;
